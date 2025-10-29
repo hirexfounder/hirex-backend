36c5d852850cfe74e8aa32116ce8e7774cddfd93
hirexfounder <hirexfounder@gmail.com>
2025-10-29 12:48:31 -0500

    Create jobs.py

---
+++ jobs.py
@@ -0,0 +1,18 @@
+# Sample job data
+def get_jobs():
+    return [
+        {
+            "title": "Software Engineer",
+            "company": "Google",
+            "location": "Mountain View, CA",
+            "sponsor": True,
+            "url": "https://careers.google.com"
+        },
+        {
+            "title": "Data Analyst",
+            "company": "Microsoft",
+            "location": "Redmond, WA",
+            "sponsor": True,
+            "url": "https://careers.microsoft.com"
+        }
+    ]
